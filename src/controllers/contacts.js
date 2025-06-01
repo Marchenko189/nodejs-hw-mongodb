3158aecb55163fdf70ef2bcd2d2cc4df7ece73b7
Marchenko189 <marcenkoo189@gmail.com>
2025-06-01 22:35:00 +0300

    h3

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -3,7 +3,8 @@ import createHttpError from 'http-errors';
 
 export const getContactsController = async (req, res) => {
     const contacts = await getAllContacts();
-    res.status(200).json({
+    res.json({
+        status: 200,
         message: "Successfully found contacts!",
         data: contacts,
     });
@@ -17,7 +18,8 @@ export const getContactByIdController = async (req, res, next) => {
         throw createHttpError(404, 'Contact not found');
     }
     
-    res.status(200).json({
+    res.json({
+        status: 200,
         message: `Successfully found contact with id ${contactId}!`,
         data: contact,
     });
@@ -26,7 +28,8 @@ export const getContactByIdController = async (req, res, next) => {
 export const createContactController = async (req, res) => {
     const contact = await createContact(req.body);
 
-    res.status(201).json({
+    res.json({
+        status: 201,
         message: "Successfully created a contact!",
         data: contact,
     });
@@ -41,7 +44,8 @@ export const patchContactController = async (req, res, next) => {
         return;
     }
 
-    res.status(200).json({
+    res.json({
+        status: 200,
         message: "Successfully patched a contact!",
         data: result,
     });
